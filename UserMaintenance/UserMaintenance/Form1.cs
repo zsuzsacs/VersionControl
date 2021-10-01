6bebe4727923424bf37b45d9401cb1b2b9bc127e
zsuzsacs <zsuzsa.cserni@stud.uni-corvinus.hu>
2021-10-02 00:00:35 +0200

    Creating UI

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -15,6 +15,10 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.Lastname;
+            label2.Text = Resource1.Firstname;
+            button1.Text = Resource1.Add;
+
         }
     }
 }
