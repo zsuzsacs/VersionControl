97b87ca6358f9a6399cf665668f2589a45b2d0c5
zsuzsacs <zsuzsa.cserni@stud.uni-corvinus.hu>
2021-10-22 12:51:23 +0200

    Load data

--- gyak5/gyak5/Form1.cs
+++ gyak5/gyak5/Form1.cs
@@ -12,9 +12,15 @@ namespace gyak5
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
