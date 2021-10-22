7142f105a33ea2d14ec99921f5b8180f1995ee88
zsuzsacs <zsuzsa.cserni@stud.uni-corvinus.hu>
2021-10-22 13:00:45 +0200

    create getportfoliovalue

--- gyak5/gyak5/Form1.cs
+++ gyak5/gyak5/Form1.cs
@@ -32,5 +32,20 @@ namespace gyak5
 
             dataGridView2.DataSource = Portfolio;
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
