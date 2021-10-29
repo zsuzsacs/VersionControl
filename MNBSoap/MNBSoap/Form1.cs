d10ed2afec1c034c40dafba6564c484e80ab7c4e
zsuzsacs <zsuzsa.cserni@stud.uni-corvinus.hu>
2021-10-29 12:45:25 +0200

    3/1,2,3,4

--- MNBSoap/MNBSoap/Form1.cs
+++ MNBSoap/MNBSoap/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MNBSoap.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,20 @@ namespace MNBSoap
         public Form1()
         {
             InitializeComponent();
+            Consume();
+        }
+
+        void Consume()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            File.WriteAllText("export.xml", result);
+
         }
     }
 }
